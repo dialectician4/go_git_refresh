ba02b710460d63f6b8862049ae65e71b629f51ae
dialectician4 <edwin.santos.kov@gmail.com>
2025-07-06 09:50:12 -0400

    Added typing stuff

--- utils.go
+++ utils.go
@@ -71,6 +71,11 @@ func deltaParse(s string) (string, error) {
 	return out_val, nil
 }
 
+func parseDelta(s string) Result[string] {
+	res, err := deltaParse(s)
+	return Result[string]{inner: res, err: err}
+}
+
 // Set RefreshCLI mode for finding paths exempt from recycling by git refresh.
 func exemptsParse(s string) (string, error) {
 	opt_mapping := map[string]string{
@@ -84,6 +89,11 @@ func exemptsParse(s string) (string, error) {
 		return "", errors.New(fmt.Sprintln("Unrecognized value for exemptions option: ", s))
 	}
 	return out_val, nil
+}
+
+func parseExempts(s string) Result[string] {
+	res, err := exemptsParse(s)
+	return Result[string]{inner: res, err: err}
 }
 
 // Take options (map keys) and their values and configure CLIConfig accordingly
@@ -274,5 +284,125 @@ func GetGitRefreshConfig() (*RefreshCLI, error) {
 		}
 	}
 	return &program_config, nil
+}
 
+func GetConfig() Result[*RefreshCLI] {
+	config, err := GetGitRefreshConfig()
+	return Result[*RefreshCLI]{inner: config, err: err}
+}
+
+type Optional[T any] struct {
+	inner   T
+	isEmpty bool
+}
+
+func Some[T any](in T) Optional[T] {
+	return Optional[T]{inner: in, isEmpty: false}
+}
+
+func None[T any]() Optional[T] {
+	return Optional[T]{isEmpty: true}
+}
+
+func (o *Optional[T]) IsSome() bool {
+	return !o.isEmpty
+}
+
+func (o *Optional[T]) IsNone() bool {
+	return o.isEmpty
+}
+
+func (o *Optional[T]) Unwrap() T {
+	if o.IsNone() {
+		panic("attempted to unwrap empty Optional!")
+	}
+	return o.inner
+}
+
+func (o *Optional[T]) UnwrapOr(fallback T) T {
+	if o.IsNone() {
+		return fallback
+	}
+	return o.inner
+}
+
+func (o *Optional[T]) Destructure() (T, error) {
+	if o.IsNone() {
+		return o.inner, errors.New("attempted to unwrap empty Optional")
+	}
+	return o.inner, nil
+}
+
+func MapOpt[T, G any](o Optional[T], fn func(T) G) Optional[G] {
+	if o.IsNone() {
+		return Optional[G]{isEmpty: true}
+	}
+	return Optional[G]{inner: fn(o.inner), isEmpty: false}
+}
+
+func FlatMapOpt[T, G any](o Optional[T], fn func(T) Optional[G]) Optional[G] {
+	if o.IsNone() {
+		return None[G]()
+	}
+	return fn(o.inner)
+}
+
+// ====Result
+
+type Empty struct{}
+
+func NewEmpty() Empty {
+	return Empty{}
+}
+
+type ErrResult Result[Empty]
+
+func AsErrResult(err error) ErrResult {
+	return ErrResult{inner: NewEmpty(), err: err}
+}
+
+type Result[T any] struct {
+	inner T
+	err   error
+}
+
+func Ok[T any](in T) Result[T] {
+	return Result[T]{inner: in, err: nil}
+}
+
+func Err[T any](err error) Result[T] {
+	return Result[T]{err: err}
+}
+
+func (r *Result[T]) IsOk() bool {
+	return r.err == nil
+}
+
+func (r *Result[T]) IsErr() bool {
+	return r.err != nil
+}
+
+func (r *Result[T]) Unwrap() T {
+	if r.IsErr() {
+		panic("attempted to unwrap error Result!")
+	}
+	return r.inner
+}
+
+func (r *Result[T]) Destructure() (T, error) {
+	return r.inner, r.err
+}
+
+func MapRes[T, G any](r Result[T], fn func(T) G) Result[G] {
+	if r.IsErr() {
+		return Result[G]{err: r.err}
+	}
+	return Result[G]{inner: fn(r.inner), err: nil}
+}
+
+func FlatMapRes[T, G any](r Result[T], fn func(T) Result[G]) Result[G] {
+	if r.IsErr() {
+		return Err[G](r.err)
+	}
+	return fn(r.inner)
 }
